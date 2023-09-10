951cccee3306d80bbc1b52809727efd523acc3e7
Daneel Malgas <daneel@deimos.co.za>
2023-09-10 14:01:45 +0200

    feat: returns Fizz when provided with 3 - red

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,4 +5,8 @@ describe("fizzbuzz", () => {
     it('should return a string', () => {
         expect(typeof fizzBuzz(1)).toBe('string');
     })
+
+    it('should return "Fizz" when provided with the value 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+    })
 });
