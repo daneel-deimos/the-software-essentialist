2dee3457c49f188c3d1ada8cf22ac83fff3739cc
Daneel Malgas <daneel@deimos.co.za>
2023-09-10 14:30:04 +0200

    feat: return a RangeError when provided with 102 - green

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,8 @@
 export function fizzBuzz(input:number) {
+    if (input < 1 || input > 100) {
+        throw new RangeError('The argument must be between 1 and 100.')
+    }
+
     if (input % 3 === 0 && input !== 15 && input !== 45) {
         return 'Fizz';
     }
