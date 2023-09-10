3517debb51a98ccc70e888fca06fe138fd75a59f
Daneel Malgas <daneel@deimos.co.za>
2023-09-10 14:38:43 +0200

    refactor: used parameterization to clean up the Fizz tests

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -6,8 +6,10 @@ describe("fizzbuzz", () => {
         expect(typeof fizzBuzz(1)).toBe('string');
     })
 
-    it('should return "Fizz" when provided with the value 3', () => {
-        expect(fizzBuzz(3)).toBe('Fizz');
+    it('should "Fizz" when provided with the values 3, 9, and 42', () => {
+        [3, 9, 42]
+            .map((input:number) => fizzBuzz(input))
+            .forEach((output) => expect(output).toBe('Fizz'));
     })
 
     it('should return "Buzz" when provided with the value 5', () => {
@@ -17,23 +19,15 @@ describe("fizzbuzz", () => {
     it('should return "FizzBuzz" when provided with the value 15', () => {
         expect(fizzBuzz(15)).toBe('FizzBuzz');
     })
-
-    it('should return "Fizz" when provided with the value 9', () => {
-        expect(fizzBuzz(9)).toBe('Fizz');
-    });
-
-    it('should return "43" when provided with the value 43', () => {
-        expect(fizzBuzz(43)).toBe('43');
-    })
-
-    it('should return "Fizz" when provided with the value 42', () => {
-        expect(fizzBuzz(42)).toBe('Fizz');
-    })
-
+    
     it('should return "FizzBuzz" when provided with the value 45', () => {
         expect(fizzBuzz(45)).toBe('FizzBuzz')
     })
 
+    it('should return "43" when provided with the value 43', () => {
+        expect(fizzBuzz(43)).toBe('43');
+    })
+    
     it('should throw a RangeError when provided with the value 102', () => {
         expect(() => fizzBuzz(102)).toThrow(RangeError);
         expect(() => fizzBuzz(102)).toThrow('The argument must be between 1 and 100.');
