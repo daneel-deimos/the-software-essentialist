a25da19ddc531599a4fe08d7b0792228892f7662
Daneel Malgas <daneel@deimos.co.za>
2023-10-25 20:24:58 +0200

    refactor: simplify the implemention by removing duplication

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,5 @@
 export function palindromeChecker(input:string) {
     const formattedInput = input.toLowerCase().replace(/ /g, "");
-    const reversed = input.toLowerCase().split("").reverse().join("").replace(/ /g, "");
+    const reversed = formattedInput.split("").reverse().join("");
     return formattedInput === reversed;
 }
